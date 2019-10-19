c017d04587867343c7621cde7fd8800d44e64fff
José Guidini <45466419+jaguidini@users.noreply.github.com>
2019-10-19 09:08:37 -0300

    Update setModel.py

--- setModel.py
+++ setModel.py
@@ -13,27 +13,31 @@ from sklearn.linear_model import LogisticRegression
 class setModel():
     def __init__(self):        
         self.__root = config.ROOT_PATH     
-        
-    #por default pega o RandomForest
-    def generateModel(self, model="RandomForest"):    
-        df = pd.read_csv(config.DIR_DATA + config.FILE_NORMAL, delimiter=';')
-       
-        attributes = df.drop('outcome_type', axis=1)
-        classes = df['outcome_type']
-        
-        X_train, X_test, y_train, y_test = train_test_split(attributes, classes, test_size=0.20)
 
-        if model.upper() == 'RandomForest'.upper():            
-            classifier = RandomForestClassifier()
-        elif model.upper() == 'DecisionTree'.upper():            
-            classifier = DecisionTreeClassifier()
-        elif model.upper() == 'LogisticRegression'.upper():            
-            classifier = LogisticRegression()            
-        else:
-            print('Classificador não encontrado, informe um dos tipos (\'RandomForest\', \'DecisionTree\', \'LogisticRegression\').\n')
-            return        
+    #por default pega o RandomForest
+    def generateModel(self, model="RandomForest", kernel="linear"):    
+        df = pd.read_csv(config.DIR_DATA + config.FILE_BASE, delimiter=';')
+
+        attributes = df.drop('adoption', axis=1)
+        classes = df['adoption']
+
+        X_train, X_test, y_train, y_test = train_test_split(attributes, classes, test_size=0.20)
         
+        if model.upper() == 'RANDOMFOREST':            
+            classifier = RandomForestClassifier()
+        elif model.upper() == 'DECISIONTREE':
+            classifier = DecisionTreeClassifier()
+        elif model.upper() == 'LOGISTICREGRESSION':
+            classifier = LogisticRegression()
+        elif (model.upper() == 'SVM'):
+            if (kernel.upper() == 'LINEAR') | (kernel.upper() == 'LINEAR') | (kernel.upper() == 'POLY'):
+                classifier = svm.SVC(kernel = kernel, C = 1, probability=True)
+        else:
+            print('Classificador não encontrado, informe um dos tipos (\'RandomForest\', \'DecisionTree\', \'LogisticRegression\' ou \'SVM\').\n')
+            return        
+
         classifier.fit(X_train, y_train)
+
         y_pred = classifier.predict(X_test)
         y_pred_proba = classifier.predict_proba(X_test)
         
@@ -49,9 +53,8 @@ class setModel():
         fileName = config.DIR_MODEL + config.FILE_MODEL.format(model)
         joblib.dump(classifier, fileName)
         print('Modelo [{0}] gerado com sucesso!'.format(fileName),'\n')
-        
 
-setModel().generateModel('RandomForest')
-setModel().generateModel('DecisionTree')
-setModel().generateModel('LogisticRegression')
-
+setModel().generateModel(model='RandomForest')
+setModel().generateModel(model='DecisionTree')
+setModel().generateModel(model='LogisticRegression')
+setModel().generateModel(model='SVM', kernel='linear')
